0fdb44f48cdea7437f9f0a511cdda7acf4034560
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-09-20 19:58:24 +0200

    cleanup

--- src/ios/SSLCertificateChecker.m
+++ src/ios/SSLCertificateChecker.m
@@ -10,11 +10,10 @@
 }
 
 -(void)check:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options {
-
     _callbackId = [arguments pop];
-    
-    NSString *serverURL = [arguments objectAtIndex:0];
-    _allowedFingerprint = [arguments objectAtIndex:1];
+
+    NSString *serverURL    = [arguments objectAtIndex:0];
+    _allowedFingerprint    = [arguments objectAtIndex:1];
     _allowedFingerprintAlt = [arguments objectAtIndex:2];
 
     // create url
@@ -24,37 +23,26 @@
     // connect
     NSURLRequest *request = [NSURLRequest requestWithURL:endpointURL];
 
-    // delegate:self causes willSendRequestForAuthenticationChallenge method below to be called
     if (![NSURLConnection connectionWithRequest:request delegate:self]) {
-        NSLog(@"SSLCertificateChecker connect result: not connected");
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"not connected"];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"CONNECTION_FAILED"];
         [self writeJavascript:[pluginResult toErrorCallbackString:_callbackId]];
     }
 }
 
-// Delegate method
+// Delegate method, called from connectionWithRequest
 - (void) connection: (NSURLConnection*)connection willSendRequestForAuthenticationChallenge: (NSURLAuthenticationChallenge*)challenge {
-    // Get the SecTrustRef from the server
-    NSURLProtectionSpace* protSpace = challenge.protectionSpace;
-    SecTrustRef trust = protSpace.serverTrust;
-    
-    // Compare SecTrustGetCertificateAtIndex[0] with predefined fingerprints of trusted certificates.
-    NSString* fingerprint = [self getFingerprint: SecTrustGetCertificateAtIndex(trust, 0)];
-    //    fingerprint = @"########## TESTINGTESTING123 ##########";
+    NSString* fingerprint = [self getFingerprint: SecTrustGetCertificateAtIndex(challenge.protectionSpace.serverTrust, 0)];
     if ([self isFingerprintTrusted: fingerprint]) {
-        NSLog(@"found match for fingerprint. Nothing on the hand, so lets continue");
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"secure"];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"CONNECTION_SECURE"];
         [self writeJavascript:[pluginResult toSuccessCallbackString:_callbackId]];
     } else {
-        // If we arrive here, then there is a connection, but it is not secure.
-        NSLog(@"no match found for fingerprint, HACKING IN PROGRESS???");
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"not secure"];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"CONNECTION_NOT_SECURE"];
         [self writeJavascript:[pluginResult toErrorCallbackString:_callbackId]];
     }
 }
 
 - (NSString*) getFingerprint: (SecCertificateRef) cert {
-    NSData* certData = (__bridge NSData*) SecCertificateCopyData(cert); // Note: added __bridge as suggested by XCode
+    NSData* certData = (__bridge NSData*) SecCertificateCopyData(cert);
     unsigned char sha1Bytes[CC_SHA1_DIGEST_LENGTH];
     CC_SHA1(certData.bytes, certData.length, sha1Bytes);
     NSMutableString *fingerprint = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 3];
@@ -65,8 +53,8 @@
 }
 
 - (BOOL) isFingerprintTrusted: (NSString*)fingerprint {
-        return ((_allowedFingerprint    != [NSNull null] && [fingerprint caseInsensitiveCompare: _allowedFingerprint]    == NSOrderedSame) ||
-                (_allowedFingerprintAlt != [NSNull null] && [fingerprint caseInsensitiveCompare: _allowedFingerprintAlt] == NSOrderedSame));
+    return ((_allowedFingerprint    != [NSNull null] && [fingerprint caseInsensitiveCompare: _allowedFingerprint]    == NSOrderedSame) ||
+            (_allowedFingerprintAlt != [NSNull null] && [fingerprint caseInsensitiveCompare: _allowedFingerprintAlt] == NSOrderedSame));
 }
 
 @end
