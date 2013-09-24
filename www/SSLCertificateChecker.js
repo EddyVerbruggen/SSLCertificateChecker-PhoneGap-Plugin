b9c7439e4061ca87ec61bbae5755589e50e5c8d6
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-09-24 20:49:23 +0200

    cleanup

--- www/SSLCertificateChecker.js
+++ www/SSLCertificateChecker.js
@@ -1,26 +1,27 @@
 "use strict";
-function SSLCertificateChecker() {}
+function SSLCertificateChecker() {
+}
 
 SSLCertificateChecker.prototype.check = function (successCallback, errorCallback, serverURL, allowedSHA1Fingerprint, allowedSHA1FingerprintAlt) {
-    if (typeof errorCallback != "function") {
-        console.log("SSLCertificateChecker.find failure: errorCallback parameter must be a function");
-        return
-    }
-    
-    if (typeof successCallback != "function") {
-        console.log("SSLCertificateChecker.find failure: successCallback parameter must be a function");
-        return
-    }
-    cordova.exec(successCallback, errorCallback, "SSLCertificateChecker", "check", [serverURL, allowedSHA1Fingerprint, allowedSHA1FingerprintAlt]);
+  if (typeof errorCallback != "function") {
+    console.log("SSLCertificateChecker.find failure: errorCallback parameter must be a function");
+    return
+  }
+
+  if (typeof successCallback != "function") {
+    console.log("SSLCertificateChecker.find failure: successCallback parameter must be a function");
+    return
+  }
+  cordova.exec(successCallback, errorCallback, "SSLCertificateChecker", "check", [serverURL, allowedSHA1Fingerprint, allowedSHA1FingerprintAlt]);
 };
 
 SSLCertificateChecker.install = function () {
-    if (!window.plugins) {
-        window.plugins = {};
-    }
-    
-    window.plugins.sslCertificateChecker = new SSLCertificateChecker();
-    return window.plugins.cameraRoll;
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.sslCertificateChecker = new SSLCertificateChecker();
+  return window.plugins.sslCertificateChecker;
 };
 
 cordova.addConstructor(SSLCertificateChecker.install);
